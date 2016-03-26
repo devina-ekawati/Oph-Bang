7ce23d2b4135d9ed47f5c213aa714d523104e043
Ivan Andrianto <andrianto.ivan@yahoo.co.id>
2016-03-26 13:30:41 +0700

    .

---
+++ Assets/Spin.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour {
+	public float speed = 0.03F;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// rotate at 90 degrees per second
+		transform.Rotate(Vector3.up * Time.deltaTime*90);
+	}
+}
