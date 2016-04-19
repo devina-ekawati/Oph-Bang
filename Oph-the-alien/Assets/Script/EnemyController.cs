346c39c3b78063a2db37235e546bec02fd659ec0
devina-ekawati <devina.ekawati@gmail.com>
2016-04-19 16:44:28 +0700

    Add sript for generate enemy

---
+++ Oph-the-alien/Assets/Script/EnemyController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+    private Vector3 startPosition;
+
+	// Use this for initialization
+	void Start () {
+        startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(startPosition.x, startPosition.y, startPosition.z + 0.01f);
+	}
+}
---
+++ Oph-the-alien/Assets/Script/GenerateEnemyController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateEnemyController : MonoBehaviour {
+
+    public GameObject enemy;
+
+    private float timeElapsed = 0;
+    private float generateCycle = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("CreateEnemy", 1f, 1.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void CreateEnemy()
+    {
+        GameObject temp = (GameObject)Instantiate(enemy);
+        Vector3 pos = temp.transform.position;
+        temp.transform.position = new Vector3(Random.Range(-3, 4), pos.y, pos.z);
+    }
+}
