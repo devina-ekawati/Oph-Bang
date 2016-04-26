15c625aa906b21b8250a3ffce785a73dd24a1c50
devina-ekawati <devina.ekawati@gmail.com>
2016-04-26 19:05:43 +0700

    Add opening scene

---
+++ Oph-the-alien/Assets/Script/OpeningStoryController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpeningStoryController : MonoBehaviour {
+    public GameObject[] openingStory;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(ChangeOpeningSceneFunction());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    IEnumerator ChangeOpeningSceneFunction()
+    {
+        for (int i = 0; i < 23; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                openingStory[j].SetActive(false);
+            }
+            
+            openingStory[i].SetActive(true);
+            yield return new WaitForSeconds(1);
+            
+        }
+    }
+
+}
