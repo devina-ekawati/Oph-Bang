dc5cbae46cb3cc16498d12a4b79e763945cb02a6
devina-ekawati <devina.ekawati@gmail.com>
2016-05-11 14:18:57 +0700

    Add fade for splash screen

--- Oph-the-alien/Assets/Script/SplashScreen.cs
+++ Oph-the-alien/Assets/Script/SplashScreen.cs
@@ -1,27 +1,46 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class SplashScreen : MonoBehaviour {
 	public float timer = 1f;
 	public string levelToLoad = "Startgame";
 	public GameObject myParentPanel;
+    private Texture2D fadeOutTexture;
+    public float fadeSpeed = 10f;
+
+    private int drawDepth = -1000;
+    private float alpha = 1.0f;
+    private int fadeDir = -1;
+    //private bool finish = false;
+
 	// Use this for initialization
 	void Start () {
-		//StartCoroutine ("DisplayScene");
+        fadeOutTexture = new Texture2D(1, 1);
+        fadeOutTexture.SetPixel(0, 0, Color.black);
+        fadeOutTexture.Apply();
+		
 	}
 
-	/*IEnumerator DisplayScene() {
-		float alpha = myParentPanel.alpha;
-		for (float t = 0.0f; t < 1.0f; t += 0.01f)
-		{
-			myParentPanel.alpha = Mathf.Lerp(alpha,1,t);
-		}
-		yield return new WaitForSeconds(timer);
-		for (float t = 0.0f; t < 0.5f; t -= 0.01f)
-		{
-			myParentPanel.alpha = Mathf.Lerp(alpha,1,t);
-		}
-		Application.LoadLevel (levelToLoad);
-	}*/
+    void OnGUI()
+    {
+        alpha += fadeDir * fadeSpeed * Time.deltaTime;
+        alpha = Mathf.Clamp01(alpha);
+
+        Color temp = GUI.color;
+        temp.a = alpha;
+        GUI.color = temp;
+
+        GUI.depth = drawDepth;
+
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fadeOutTexture);
+
+        if (alpha == 0)
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+
+        
+    }
 
 }
