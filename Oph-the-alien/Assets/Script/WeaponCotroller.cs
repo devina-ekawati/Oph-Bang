46c49431e69287959aa7361c259209800025fe7b
devina-ekawati <devina.ekawati@gmail.com>
2016-05-11 12:16:34 +0700

    Fix conflict

---
+++ Oph-the-alien/Assets/Script/WeaponCotroller.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponCotroller : MonoBehaviour {
+
+	private Vector3 startPosition;
+	
+	// Use this for initialization
+	void Start () {
+		startPosition = transform.position;
+		StartCoroutine(MoveWeaponFunction());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	IEnumerator MoveWeaponFunction()
+	{
+		yield return new WaitForSeconds(0.05f);
+		MoveWeapon();
+		StartCoroutine(MoveWeaponFunction());
+	}
+	
+	void MoveWeapon()
+	{
+		transform.Translate(0,0.02f, 0.3f);
+	}
+	
+}
